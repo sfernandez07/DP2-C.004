59432d1bd62e57fe7a2d2dee367180aad6bbc73c
GuilleGL <guillegarcialeon12@gmail.com>
2025-02-28 11:15:04 +0100

    Fix: Añadir temporal(TIMESTAMP) en atributo moment de Claim

--- src/main/java/acme/entities/claims/Claim.java
+++ src/main/java/acme/entities/claims/Claim.java
@@ -5,6 +5,8 @@ import java.util.Date;
 
 import javax.persistence.Entity;
 import javax.persistence.ManyToOne;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
 import javax.validation.Valid;
 
 import acme.client.components.basis.AbstractEntity;
@@ -30,7 +32,7 @@ public class Claim extends AbstractEntity {
 
 	@Mandatory
 	@ValidMoment(past = true)
-	@Automapped
+	@Temporal(TemporalType.TIMESTAMP)
 	private Date				registrationMoment;
 
 	@Mandatory
