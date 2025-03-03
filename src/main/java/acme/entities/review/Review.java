dfa21fee303a10a5bda8912ab2da12911d524b09
sfernandez07 <sanferrom@alum.us.es>
2025-03-03 20:25:46 +0100

    feat: Primera versión

---
+++ src/main/java/acme/entities/review/Review.java
@@ -0,0 +1,59 @@
+
+package acme.entities.review;
+
+import java.util.Date;
+
+import javax.persistence.Entity;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+import javax.validation.Valid;
+
+import acme.client.components.basis.AbstractEntity;
+import acme.client.components.mappings.Automapped;
+import acme.client.components.validation.Mandatory;
+import acme.client.components.validation.ValidMoment;
+import acme.client.components.validation.ValidNumber;
+import acme.client.components.validation.ValidString;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+public class Review extends AbstractEntity {
+
+	/**
+	 * 
+	 */
+	private static final long	serialVersionUID	= 1L;
+
+	@Mandatory
+	@ValidString(max = 50)
+	@Automapped
+	private String				name;
+
+	@Mandatory
+	@ValidMoment(past = true)
+	@Temporal(TemporalType.TIMESTAMP)
+	private Date				momentPosted;
+
+	@Mandatory
+	@ValidString(max = 50)
+	@Automapped
+	private String				subject;
+
+	@Mandatory
+	@ValidString(max = 255)
+	@Automapped
+	private String				text;
+
+	@Mandatory
+	@ValidNumber(min = 0, max = 10)
+	@Automapped
+	private Double				score;
+
+	@Mandatory
+	@Valid
+	@Automapped
+	private Boolean				recommended;
+}
