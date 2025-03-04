4295625ad6345c520f41e9fff9c250e07e124272
sfernandez07 <sanferrom@alum.us.es>
2025-03-04 22:09:31 +0100

    fix: Cambios tras comentarios en la review

--- src/main/java/acme/entities/review/Review.java
+++ src/main/java/acme/entities/review/Review.java
@@ -11,6 +11,7 @@ import javax.validation.Valid;
 import acme.client.components.basis.AbstractEntity;
 import acme.client.components.mappings.Automapped;
 import acme.client.components.validation.Mandatory;
+import acme.client.components.validation.Optional;
 import acme.client.components.validation.ValidMoment;
 import acme.client.components.validation.ValidNumber;
 import acme.client.components.validation.ValidString;
@@ -47,12 +48,12 @@ public class Review extends AbstractEntity {
 	@Automapped
 	private String				text;
 
-	@Mandatory
+	@Optional
 	@ValidNumber(min = 0, max = 10)
 	@Automapped
 	private Double				score;
 
-	@Mandatory
+	@Optional
 	@Valid
 	@Automapped
 	private Boolean				recommended;
