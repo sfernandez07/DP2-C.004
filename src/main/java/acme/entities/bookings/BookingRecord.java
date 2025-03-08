3089cc2c2da0aef78797fa2ec3e035f5ab8ed370
sfernandez07 <sanferrom@alum.us.es>
2025-03-08 18:28:13 +0100

    fix: Cambios tras comentarios en foro

---
+++ src/main/java/acme/entities/bookings/BookingRecord.java
@@ -0,0 +1,34 @@
+
+package acme.entities.bookings;
+
+import javax.persistence.Entity;
+import javax.persistence.ManyToOne;
+import javax.validation.Valid;
+
+import acme.client.components.basis.AbstractEntity;
+import acme.client.components.validation.Mandatory;
+import acme.entities.passengers.Passenger;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+public class BookingRecord extends AbstractEntity {
+
+	/**
+	 * 
+	 */
+	private static final long	serialVersionUID	= 1L;
+
+	@Mandatory
+	@ManyToOne
+	@Valid
+	private Booking				booking;
+
+	@Mandatory
+	@ManyToOne
+	@Valid
+	private Passenger			passenger;
+
+}
