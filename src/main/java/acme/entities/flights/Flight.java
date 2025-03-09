6dfd86b64c6865eecbe40b03304a81b80c58c1df
Alberto1902 <Alberto1902>
2025-03-09 20:22:38 +0100

    flight fixed sin NotNull, mas validación y sin la relacion que usaba OneToMany

--- src/main/java/acme/entities/flights/Flight.java
+++ src/main/java/acme/entities/flights/Flight.java
@@ -2,21 +2,18 @@
 package acme.entities.flights;
 
 import java.util.Date;
-import java.util.List;
 
 import javax.persistence.Column;
-import javax.persistence.OneToMany;
 import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
-import javax.validation.Valid;
 import javax.validation.constraints.DecimalMax;
 import javax.validation.constraints.DecimalMin;
-import javax.validation.constraints.NotNull;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
 
 import acme.client.components.basis.AbstractEntity;
 import acme.client.components.validation.Mandatory;
 import acme.client.components.validation.Optional;
-import acme.client.components.validation.ValidString;
 
 public class Flight extends AbstractEntity {
 
@@ -26,48 +23,55 @@ public class Flight extends AbstractEntity {
 
 	// Attributes -------------------------------------------------------------
 
+	@NotBlank
 	@Mandatory
-	@ValidString(min = 1, max = 50)
+	@Size(min = 1, max = 50)
 	@Column(nullable = false)
 	private String				tag;
 
+	@Column(nullable = false)
 	@Mandatory
 	private Boolean				selfTransfer;
 
 	@Mandatory
 	@DecimalMin(value = "1.00")
 	@DecimalMax(value = "1000000.00")
+	@Column(nullable = false)
 	private Double				cost;
 
-	@ValidString
+	@Size(max = 255)
+	@Column
 	@Optional
 	private String				description;
 
 	// Derived attributes -----------------------------------------------------
 
 	@Temporal(TemporalType.TIMESTAMP)
-	@NotNull
+	@Column(nullable = false)
 	@Mandatory
 	private Date				scheduleDeparture;
 
 	@Temporal(TemporalType.TIMESTAMP)
+	@Column(nullable = false)
 	@Mandatory
 	private Date				scheduleArrival;
 
+	@NotBlank
+	@Size(max = 100)
+	@Column(nullable = false, length = 100)
 	@Mandatory
 	private String				originCity;
 
+	@NotBlank
+	@Size(max = 100)
+	@Column(nullable = false, length = 100)
 	@Mandatory
 	private String				destinationCity;
 
+	@Column(nullable = false)
 	@Mandatory
 	private Integer				layovers;
 
 	// Relationships ----------------------------------------------------------
-
-	@OneToMany
-	@Mandatory
-	@Valid
-	private List<FlightLeg>		flightLegs;
 
 }
