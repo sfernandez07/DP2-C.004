2f8ccd878a101fbff1061fa7b5897fcd5558b6e1
Alberto1902 <Alberto1902>
2025-03-09 20:42:45 +0100

    añadidas validaciones a la entidad y completada

--- src/main/java/acme/entities/flights/FlightLeg.java
+++ src/main/java/acme/entities/flights/FlightLeg.java
@@ -3,8 +3,14 @@ package acme.entities.flights;
 
 import java.util.Date;
 
+import javax.persistence.Column;
 import javax.persistence.ManyToOne;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
 import javax.validation.Valid;
+import javax.validation.constraints.DecimalMin;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
 
 import acme.client.components.basis.AbstractEntity;
 import acme.client.components.validation.Mandatory;
@@ -17,20 +23,45 @@ public class FlightLeg extends AbstractEntity {
 
 	// Attributes -------------------------------------------------------------
 
+	@Column(unique = true, nullable = false)
+	@Mandatory
 	private Integer				flightNumber;
 
+	@Temporal(TemporalType.TIMESTAMP)
+	@Column(nullable = false)
+	@Mandatory
 	private Date				scheduleDeparture;
 
+	@Temporal(TemporalType.TIMESTAMP)
+	@Column(nullable = false)
+	@Mandatory
 	private Date				scheduleArrival;
 
+	@DecimalMin(value = "0.0")
+	@Column(nullable = false)
+	@Mandatory
 	private Double				durationHours;
 
+	@Column(nullable = false)
+	@Mandatory
 	private LegStatus			status;
 
+	@NotBlank
+	@Size(max = 100)
+	@Column(nullable = false)
+	@Mandatory
 	private String				departureAirport;
 
+	@NotBlank
+	@Size(max = 100)
+	@Column(nullable = false)
+	@Mandatory
 	private String				arrivalAirport;
 
+	@NotBlank
+	@Size(max = 100)
+	@Column(nullable = false)
+	@Mandatory
 	private String				aircraft;
 
 	// Derived attributes -----------------------------------------------------
