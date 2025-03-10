cf9bc74fdfaba7ef6add37eb09b9b918bfdb5ac7
sfernandez07 <sanferrom@alum.us.es>
2025-03-10 20:47:29 +0100

    feat: Progreso testing passengger

--- src/main/java/acme/entities/passengers/Passenger.java
+++ src/main/java/acme/entities/passengers/Passenger.java
@@ -11,6 +11,7 @@ import acme.client.components.basis.AbstractEntity;
 import acme.client.components.mappings.Automapped;
 import acme.client.components.validation.Mandatory;
 import acme.client.components.validation.Optional;
+import acme.client.components.validation.ValidEmail;
 import acme.client.components.validation.ValidMoment;
 import acme.client.components.validation.ValidString;
 import lombok.Getter;
@@ -32,7 +33,7 @@ public class Passenger extends AbstractEntity {
 	private String				fullName;
 
 	@Mandatory
-	@ValidString
+	@ValidEmail
 	@Automapped
 	private String				email;
 
