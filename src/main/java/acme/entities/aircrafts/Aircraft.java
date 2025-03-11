452f53a2838439b2527ab4828fa19e235c77cbd1
GuilleGL <guillegarcialeon12@gmail.com>
2025-03-11 11:12:48 +0100

    fix: Añadir relacion aircraft-airline y restriccion de capacity.

--- src/main/java/acme/entities/aircrafts/Aircraft.java
+++ src/main/java/acme/entities/aircrafts/Aircraft.java
@@ -3,6 +3,7 @@ package acme.entities.aircrafts;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.ManyToOne;
 import javax.validation.Valid;
 
 import acme.client.components.basis.AbstractEntity;
@@ -11,6 +12,7 @@ import acme.client.components.validation.Mandatory;
 import acme.client.components.validation.Optional;
 import acme.client.components.validation.ValidNumber;
 import acme.client.components.validation.ValidString;
+import acme.entities.airlines.Airline;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -36,7 +38,7 @@ public class Aircraft extends AbstractEntity {
 	private Integer				registrationNumber;
 
 	@Mandatory
-	@ValidNumber
+	@ValidNumber(min = 0)
 	@Automapped
 	private Integer				capacity;
 
@@ -54,4 +56,13 @@ public class Aircraft extends AbstractEntity {
 	@ValidString(max = 255)
 	@Automapped
 	private String				details;
+
+	// Derived attributes -----------------------------------------------------
+
+	// Relationships ----------------------------------------------------------
+
+	@Mandatory
+	@Valid
+	@ManyToOne(optional = false)
+	private Airline				airline;
 }
