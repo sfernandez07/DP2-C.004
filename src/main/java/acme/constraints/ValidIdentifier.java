adb221d7c4caaf4cd06f86116269a81fe9dcf9fe
sfernandez07 <sanferrom@alum.us.es>
2025-03-11 12:55:05 +0100

    fix: Mas cambios

---
+++ src/main/java/acme/constraints/ValidIdentifier.java
@@ -0,0 +1,28 @@
+
+package acme.constraints;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+import javax.validation.Constraint;
+import javax.validation.Payload;
+import javax.validation.ReportAsSingleViolation;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Pattern;
+
+@Target(ElementType.FIELD)
+@Retention(RetentionPolicy.RUNTIME)
+@Constraint(validatedBy = {})
+@ReportAsSingleViolation
+
+@NotBlank
+@Pattern(regexp = "^[A-Z]{2-3}\\d{6}$")
+
+public @interface ValidIdentifier {
+
+	String message() default "El identificador debe seguir el patron";
+	Class<?>[] groups() default {};
+	Class<? extends Payload>[] payload() default {};
+}
