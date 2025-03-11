6c4d248c44256f4885e11f1adcd757da064786f0
davdelpal1 <delgado.pallares.david@gmail.com>
2025-03-11 01:04:07 +0100

    fix: Cambiando de ubicación los archivos

--- src/main/java/acme/entities/airlines/Airline.java
+++ src/main/java/acme/entities/airlines/Airline.java
@@ -1,5 +1,5 @@
 
-package acme.entities;
+package acme.entities.airlines;
 
 import java.util.Date;
 import java.util.Objects;
--- src/main/java/acme/entities/airlines/AirlineType.java
+++ src/main/java/acme/entities/airlines/AirlineType.java
@@ -1,5 +1,5 @@
 
-package acme.entities;
+package acme.entities.airlines;
 
 public enum AirlineType {
 	LUXURY, STANDARD, LOWCOST;
