26177e19f1b5c8ff76773adf16d7b3ba4c5a7b64
GuilleGL <guillegarcialeon12@gmail.com>
2025-03-13 09:57:16 +0100

    fix: Añadida relacion Agent con Airline.

--- src/main/java/acme/realms/AssistanceAgent.java
+++ src/main/java/acme/realms/AssistanceAgent.java
@@ -16,8 +16,10 @@ import java.util.Date;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.ManyToOne;
 import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
+import javax.validation.Valid;
 
 import acme.client.components.basis.AbstractRole;
 import acme.client.components.datatypes.Money;
@@ -29,6 +31,7 @@ import acme.client.components.validation.ValidMoney;
 import acme.client.components.validation.ValidString;
 import acme.client.components.validation.ValidUrl;
 import acme.constraints.ValidAssistanceAgent;
+import acme.entities.airlines.Airline;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -55,11 +58,6 @@ public class AssistanceAgent extends AbstractRole {
 	private String				languages;
 
 	@Mandatory
-	@ValidString
-	@Automapped
-	private String				airline;
-
-	@Mandatory
 	@ValidMoment(past = true)
 	@Temporal(TemporalType.TIMESTAMP)
 	private Date				airlineHiringDate;
@@ -83,4 +81,8 @@ public class AssistanceAgent extends AbstractRole {
 
 	// Relationships ----------------------------------------------------------
 
+	@Mandatory
+	@Valid
+	@ManyToOne(optional = false)
+	private Airline				airline;
 }
