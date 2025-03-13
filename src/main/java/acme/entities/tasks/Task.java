16ad7db761e20b26b82263e41e0ab92ed7e1d7f6
davdelpal1 <delgado.pallares.david@gmail.com>
2025-03-13 14:04:58 +0100

    feat: Añadiendo archivos csv para el testing

--- src/main/java/acme/entities/tasks/Task.java
+++ src/main/java/acme/entities/tasks/Task.java
@@ -1,6 +1,7 @@
 
 package acme.entities.tasks;
 
+import javax.persistence.Entity;
 import javax.persistence.ManyToOne;
 import javax.validation.Valid;
 
@@ -11,7 +12,12 @@ import acme.client.components.validation.ValidNumber;
 import acme.client.components.validation.ValidString;
 import acme.entities.maintenanceRecords.MaintenanceRecord;
 import acme.realms.Technician;
+import lombok.Getter;
+import lombok.Setter;
 
+@Entity
+@Getter
+@Setter
 public class Task extends AbstractEntity {
 
 	private static final long	serialVersionUID	= 1L;
