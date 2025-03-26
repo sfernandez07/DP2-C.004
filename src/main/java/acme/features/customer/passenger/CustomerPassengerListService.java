8e0b071c7e7e021a94b4a20810c50ccfa9f575d0
sfernandez07 <sanferrom@alum.us.es>
2025-03-26 23:42:37 +0100

    feat: En principio lista de passengers hecha a falta de arreglar la query

--- src/main/java/acme/features/customer/passenger/CustomerPassengerListService.java
+++ src/main/java/acme/features/customer/passenger/CustomerPassengerListService.java
@@ -1,6 +1,11 @@
 
 package acme.features.customer.passenger;
 
+import java.util.Collection;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+import acme.client.components.models.Dataset;
 import acme.client.services.AbstractGuiService;
 import acme.client.services.GuiService;
 import acme.entities.passengers.Passenger;
@@ -9,4 +14,29 @@ import acme.realms.Customer;
 @GuiService
 public class CustomerPassengerListService extends AbstractGuiService<Customer, Passenger> {
 
+	@Autowired
+	private CustomerPassengerRepository repository;
+
+
+	@Override
+	public void authorise() {
+		super.getResponse().setAuthorised(true);
+	}
+
+	@Override
+	public void load() {
+		Integer id = super.getRequest().getPrincipal().getActiveRealm().getId();
+		Collection<Passenger> passengers = this.repository.findPassengersByBookingId(id);
+		super.getBuffer().addData(passengers);
+	}
+
+	@Override
+	public void unbind(final Passenger passenger) {
+		Dataset dataset;
+
+		dataset = super.unbindObject(passenger, "fullName", "email", "passportNumber");
+
+		super.getResponse().addData(dataset);
+
+	}
 }
---
+++ src/main/resources/WEB-INF/views/customer/passenger/list.jsp
@@ -0,0 +1,10 @@
+<%@page%>
+
+<%@taglib prefix="jstl" uri="http://java.sun.com/jsp/jstl/core"%>
+<%@taglib prefix="acme" uri="http://acme-framework.org/"%>
+
+<acme:list>
+	<acme:list-column code="customer.passenger.list.label.fullName" path="fullName" width="33%"/>
+	<acme:list-column code="customer.passenger.list.label.email" path="email" width="33%"/>
+	<acme:list-column code="customer.passenger.list.label.passportNumber" path="passportNumber" width="34%"/>
+</acme:list>
